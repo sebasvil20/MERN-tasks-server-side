c221c21d8fa5a5440db35b7c62139b0f77dbc6a6
Sebastian Villegas <sebasvil20@gmail.com>
2020-10-16 17:50:07 -0500

    Inicializando servidor

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const conectarDB = require("./config/db");
+
+// Crear el servidor
+const app = express();
+
+// Conectar a la base de datos
+conectarDB();
+
+// Puerto de la app
+const PORT = process.env.PORT || 4000;
+
+// Arrancar la app
+app.listen(PORT, () => {
+  console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+});
