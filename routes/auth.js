62c0a6f3a4a0a4232fc8114d361c0a3c395eb7a7
Sebastian Villegas <sebasvil20@gmail.com>
2020-10-28 14:12:44 -0500

    Cambios menores
    Se cambian un poco los mensajes y se quitan validaciones innecesarias del lado del servidor

--- routes/auth.js
+++ routes/auth.js
@@ -3,15 +3,12 @@ const express = require("express");
 const router = express.Router();
 const { check } = require("express-validator");
 const authController = require("../controllers/authController");
+const auth = require("../middleware/auth");
 
-// Valida un usuario
+// Iniciar sesion
 // /api/v1/auth
-router.post("/", [
-  check("email", "Agrega un email valido").isEmail(),
-  check("password", "El password debe ser minimo de 6 caracteres").isLength({
-    min: 6,
-  }),
-  authController.autenticarUsuario,
-]);
+router.post("/", authController.autenticarUsuario);
+
+router.get("/", auth, authController.usuarioAutenticado);
 
 module.exports = router;
