6f1c85382e302158f55a43dc073c111a32019e9c
Sebastian Villegas <sebasvil20@gmail.com>
2020-11-18 07:30:09 -0500

    deployment2

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 // Puerto de la app
-const port = process.env.port || 4000;
+const port = process.env.PORT || 5000;
 
 //Importar rutas
 app.use("/api/v1/usuarios", require("./routes/usuarios"));
